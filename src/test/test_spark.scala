6b609ec1acefc40c66dd923a59285fd4d1c62b49
LHT2019 <2944381154@qq.com>
2019-01-10 11:54:13 +0800

    测试 spark 环境

--- src/test/test_spark.scala
+++ src/test/test_spark.scala
@@ -9,7 +9,8 @@ object test_spark {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("test_spark").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val data = sc.textFile("./data/wc.txt")
+    sc.setLogLevel("WARN")
+    val data = sc.textFile("./data/wc.txt").cache()
     data.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).foreach(
       word =>{
         println(word)
